b3df4f77b758edb36d7fc492d821a8c6097266e7
Charles J Liao <cjlia1@student.monash.edu>
2021-08-04 22:43:14 +1000

    Hello world print successful

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //this is a test comment
+
+        System.out.println("Hello World!");
     }
 }
